5f0d84dff9df44cb03bac94866d3e9b922db05c8
Dandi <dandi.juliandi@nawadata.id>
2024-02-18 16:28:47 +0700

    add new field for views table

---
+++ seeders/20240218083441-insert_view_field_to_fields_table.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const dayjs = require('dayjs');
+
+const dataType = require('../constats/dataType');
+const inputType = require('../constats/inputType');
+const moduleId = require('../constats/moduleId');
+const datetimeFormat = require('../constats/datetimeFormat');
+
+const now = dayjs().format(datetimeFormat.datetime.value);
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface) {
+        await queryInterface.bulkInsert(
+            'Fields',
+            [
+                {
+                    moduleId: moduleId.views,
+                    name: 'label',
+                    label: 'Label',
+                    inputType: inputType.shortText,
+                    dataType: dataType.varchar,
+                    tableRef: null,
+                    tableRefKey: null,
+                    tableRefName: null,
+                    tableRefAlias: null,
+                    tableRefFilter: null,
+                    regex: null,
+                    multiSelect: false,
+                    identity: false,
+                    notNull: false,
+                    unique: false,
+                    createdAt: now,
+                    updatedAt: now,
+                },
+                {
+                    moduleId: moduleId.views,
+                    name: 'page',
+                    label: 'Page',
+                    inputType: inputType.code,
+                    dataType: dataType.json,
+                    tableRef: null,
+                    tableRefKey: null,
+                    tableRefName: null,
+                    tableRefAlias: null,
+                    tableRefFilter: null,
+                    regex: null,
+                    multiSelect: false,
+                    identity: false,
+                    notNull: false,
+                    unique: false,
+                    createdAt: now,
+                    updatedAt: now,
+                },
+            ],
+            {},
+        );
+    },
+
+    async down(queryInterface) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('Fields', null, {});
+    },
+};
