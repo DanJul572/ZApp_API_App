309caf1743692d83f155976837fcf4b307caf265
Dandi <dandi.juliandi@nawadata.id>
2024-02-18 19:29:31 +0700

    change column type from string to json for page in views

--- migrations/20240218082738-add_page_and_label_to_views_table.js
+++ migrations/20240218082738-add_page_and_label_to_views_table.js
@@ -16,7 +16,7 @@ module.exports = {
             }),
             queryInterface.addColumn('Views', 'page', {
                 allowNull: true,
-                type: Sequelize.STRING,
+                type: Sequelize.JSON,
             }),
         ]);
     },
