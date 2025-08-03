595cc3a353204010161ecb56cdc667868b3747ad
Dandi <juliandidandi4@gmail.com>
2025-08-03 08:42:15 +0700

    use enums for status code in middleware

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -5,12 +5,12 @@ const enums = require('@enums');
 function authenticateToken(req, res, next) {
   const token = req.header('Authorization');
   if (!token) {
-    return res.sendStatus(401);
+    return res.sendStatus(enums.statusCode.UNAUTHORIZED);
   }
 
   jwt.verify(token, enums.auth.secretKey, (err, user) => {
     if (err) {
-      return res.sendStatus(403);
+      return res.sendStatus(enums.statusCode.FORBIDDEN);
     }
     req.user = user;
     next();
--- middleware/parseJsonData.js
+++ middleware/parseJsonData.js
@@ -1,10 +1,14 @@
+const enums = require('@enums');
+
 function parseJsonData(req, res, next) {
   if (req.body && typeof req.body.data === 'string') {
     try {
       const parsed = JSON.parse(req.body.data);
       req.body = parsed;
     } catch {
-      return res.status(400).json({message: 'Invalid JSON format in "data" field'});
+      return res
+        .status(enums.statusCode.BAD_REQUEST)
+        .json({message: 'Invalid JSON format in "data" field'});
     }
   }
   next();
