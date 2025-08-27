c48f006b9130c7b812f4fabf7dc37ff37e4f7967
Dandi Juliandi <dandi.julaindi@nawadata.com>
2025-08-27 10:19:03 +0700

    code formating

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,33 +1,35 @@
-const glob = require("glob");
-const esbuild = require("esbuild");
-const alias = require("esbuild-plugin-alias");
-const path = require("path");
+const glob = require('glob');
+const esbuild = require('esbuild');
+const alias = require('esbuild-plugin-alias');
+const path = require('path');
 
-const entryPoints = glob.sync("src/**/*.js"); // ambil semua file js dalam src
+const entryPoints = glob.sync('src/**/*.js'); // ambil semua file js dalam src
 
-esbuild.build({
-  entryPoints,
-  outdir: "dist",
-  platform: "node",
-  format: "cjs",
-  bundle: false,
-  sourcemap: true,
-  outbase: "src", // supaya struktur folder tetap sama
-  plugins: [
-    alias({
-      "@builders": path.resolve(__dirname, "src/builders"),
-      "@config": path.resolve(__dirname, "src/config"),
-      "@controllers": path.resolve(__dirname, "src/controllers"),
-      "@enums": path.resolve(__dirname, "src/enums"),
-      "@helpers": path.resolve(__dirname, "src/helpers"),
-      "@middleware": path.resolve(__dirname, "src/middleware"),
-      "@migrations": path.resolve(__dirname, "src/migrations"),
-      "@models": path.resolve(__dirname, "src/models"),
-      "@queries": path.resolve(__dirname, "src/queries"),
-      "@routes": path.resolve(__dirname, "src/routes"),
-      "@seeders": path.resolve(__dirname, "src/seeders"),
-      "@services": path.resolve(__dirname, "src/services"),
-      "@validations": path.resolve(__dirname, "src/validations"),
-    }),
-  ],
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    entryPoints,
+    outdir: 'dist',
+    platform: 'node',
+    format: 'cjs',
+    bundle: false,
+    sourcemap: true,
+    outbase: 'src', // supaya struktur folder tetap sama
+    plugins: [
+      alias({
+        '@builders': path.resolve(__dirname, 'src/builders'),
+        '@config': path.resolve(__dirname, 'src/config'),
+        '@controllers': path.resolve(__dirname, 'src/controllers'),
+        '@enums': path.resolve(__dirname, 'src/enums'),
+        '@helpers': path.resolve(__dirname, 'src/helpers'),
+        '@middleware': path.resolve(__dirname, 'src/middleware'),
+        '@migrations': path.resolve(__dirname, 'src/migrations'),
+        '@models': path.resolve(__dirname, 'src/models'),
+        '@queries': path.resolve(__dirname, 'src/queries'),
+        '@routes': path.resolve(__dirname, 'src/routes'),
+        '@seeders': path.resolve(__dirname, 'src/seeders'),
+        '@services': path.resolve(__dirname, 'src/services'),
+        '@validations': path.resolve(__dirname, 'src/validations'),
+      }),
+    ],
+  })
+  .catch(() => process.exit(1));
