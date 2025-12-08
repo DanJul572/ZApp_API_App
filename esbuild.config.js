764720562309590a6ec562d267323139a6fe63a6
Dandi Juliandi <dandi.julaindi@nawadata.com>
2025-12-08 10:17:26 +0700

    change esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -9,31 +9,6 @@ esbuild
     target: 'node22',
     minify: false,
     sourcemap: true,
-    external: [
-      // ORM & DB drivers
-      'sequelize',
-      'sequelize-cli',
-      'pg',
-      'pg-hstore',
-      'mysql2',
-      'sqlite3',
-
-      // Express ecosystem
-      'express',
-      'morgan',
-      'ejs',
-
-      // Validation & Swagger
-      'swagger-jsdoc',
-      'swagger-ui-express',
-      'express-validation',
-      'joi',
-
-      // Optional but recommended
-      'multer',
-      'dayjs',
-      'jsonwebtoken',
-      'bcryptjs',
-    ],
+    external: ['pg', 'mysql2', 'sqlite3', 'sequelize-cli'],
   })
   .catch(() => process.exit(1));
