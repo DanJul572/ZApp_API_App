7e344880ffc8eb107177aac743d92f0cc4261d07
Dandi <juliandidandi4@gmail.com>
2026-01-17 11:55:12 +0700

    remove access_token when 401 dan 403 status

--- src/middleware/authenticateToken.js
+++ src/middleware/authenticateToken.js
@@ -6,11 +6,13 @@ function authenticateToken(req, res, next) {
   const token = req.cookies.access_token;
 
   if (!token) {
+    res.clearCookie('access_token');
     return res.sendStatus(enums.statusCode.UNAUTHORIZED);
   }
 
   jwt.verify(token, jwtConfig.secretKey, (err, user) => {
     if (err) {
+      res.clearCookie('access_token');
       return res.sendStatus(enums.statusCode.FORBIDDEN);
     }
     req.user = user;
