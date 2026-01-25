34232a197975ce80ef48b4ef66a4dacd6042ec14
Dandi <juliandidandi4@gmail.com>
2026-01-25 08:18:02 +0700

    update errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -6,15 +6,14 @@ function errorHandler(err, _req, res) {
   if (err instanceof ValidationError) {
     return res.status(err.statusCode).json({
       success: false,
-      message: 'Validation error',
-      errors: err.details,
+      message: err.message,
+      data: err.details,
     });
   }
 
   return res.status(enums.statusCode.INTERNAL_SERVER_ERROR).json({
     success: false,
-    message: 'Internal server error',
-    error: err.message,
+    message: err.message,
   });
 }
 
