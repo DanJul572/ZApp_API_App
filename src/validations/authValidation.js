9b4aeb7418dd45db417b71eba679c5ebbebb912d
DanJul572 <juliandidandi4@gmail.com>
2026-02-20 05:04:19 +0700

    change validation error

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -2,8 +2,14 @@ const { Joi } = require('express-validation');
 
 const login = {
   body: Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    email: Joi.string().email().required().messages({
+      'string.email': 'Email format is invalid',
+      'any.required': 'Email is required',
+    }),
+
+    password: Joi.string().required().messages({
+      'any.required': 'Password is required',
+    }),
   }),
 };
 
--- src/validations/reportvalidation.js
+++ src/validations/reportvalidation.js
@@ -2,15 +2,34 @@ const { Joi } = require('express-validation');
 
 const getjsReport = {
   query: Joi.object({
-    type: Joi.string().valid('pdf', 'xlsx', 'docx', 'html').required(),
-    name: Joi.string().min(1).required(),
-    dataSchemaId: Joi.number().integer().positive().required(),
+    type: Joi.string().valid('pdf', 'xlsx', 'docx', 'html').required().messages({
+      'any.required': 'Report type is required',
+      'any.only': 'Report type must be one of pdf, xlsx, docx, or html',
+    }),
+
+    name: Joi.string().min(1).required().messages({
+      'any.required': 'Report name is required',
+      'string.empty': 'Report name cannot be empty',
+      'string.min': 'Report name cannot be empty',
+    }),
+
+    dataSchemaId: Joi.number().integer().positive().required().messages({
+      'any.required': 'Data schema id is required',
+      'number.base': 'Data schema id must be a number',
+      'number.integer': 'Data schema id must be an integer',
+      'number.positive': 'Data schema id must be a positive number',
+    }),
   }).options({ abortEarly: false }),
 };
 
 const getPreviewDataSchema = {
   query: Joi.object({
-    dataSchemaId: Joi.number().integer().positive().required(),
+    dataSchemaId: Joi.number().integer().positive().required().messages({
+      'any.required': 'Data schema id is required',
+      'number.base': 'Data schema id must be a number',
+      'number.integer': 'Data schema id must be an integer',
+      'number.positive': 'Data schema id must be a positive number',
+    }),
   }).options({ abortEarly: false }),
 };
 
